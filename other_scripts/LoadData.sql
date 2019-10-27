facf996b7156f02d5d7038360c82076c62e8b93e
Jon Jaussi <jon@jaussiconsulting.net>
2019-10-27 15:30:55 -0600

    Added Player Fact table

--- other_scripts/LoadData.sql
+++ other_scripts/LoadData.sql
@@ -6,6 +6,7 @@ MODIFICATION LOG:
 Ver      Date        Author        Description
 -----   ----------   -----------   -------------------------------------------------------------------------------
 1.0     08/02/2019   JJAUSSI       1. Built this table for LDS BC IT240
+1.1     10/27/2019   JJAUSSI       1. Added fact table load for LDS BC IT243
 
 
 RUNTIME: 
@@ -99,9 +100,37 @@ SELECT DISTINCT
      , d.t_code
   FROM dbo.tblData d
  ORDER BY d.t_id;
+ 
 
 
+-- 4) Load the Player Fact table
 
+SELECT p.pl_id 
+     , FLOOR(RAND(CHECKSUM(NEWID()))*(200000-10000+1)+10000) as mtd_salary
+	 INTO #TempTable
+  FROM dbo.tblPlayerDim AS p;
 
+WITH s1
+AS
+(
+SELECT '2019-12-31' as as_of_date UNION
+SELECT '2019-11-30' as as_of_date UNION
+SELECT '2019-10-31' as as_of_date UNION
+SELECT '2019-09-30' as as_of_date UNION
+SELECT '2019-08-31' as as_of_date UNION
+SELECT '2019-07-31' as as_of_date UNION
+SELECT '2019-06-30' as as_of_date UNION
+SELECT '2019-05-31' as as_of_date UNION
+SELECT '2019-04-30' as as_of_date UNION
+SELECT '2019-03-31' as as_of_date UNION
+SELECT '2019-02-28' as as_of_date UNION
+SELECT '2019-01-31' as as_of_date
+)
+INSERT INTO dbo.tblPlayerFact
+SELECT s1.as_of_date
+     , t.pl_id
+	 , t.mtd_salary
+  FROM s1 CROSS JOIN #TempTable AS t
+ ORDER BY 1, 2;
 
-
+DROP TABLE #TempTable;
---
+++ tables/dbo.tblPlayerFact.sql
@@ -0,0 +1,50 @@
+/*****************************************************************************************************************
+NAME:    dbo.tblPlayerFact
+PURPOSE: Create the dbo.tblPlayerFact table
+
+MODIFICATION LOG:
+Ver      Date        Author        Description
+-----   ----------   -----------   -------------------------------------------------------------------------------
+1.0     10/27/2019   JJAUSSI       1. Built this table for LDS BC IT243
+
+
+RUNTIME: 
+Approx. 0 min
+
+NOTES:
+The Player Dimension tables hold the Player attributes
+
+LICENSE: This code is covered by the GNU General Public License which guarantees end users
+the freedom to run, study, share, and modify the code. This license grants the recipients
+of the code the rights of the Free Software Definition. All derivative work can only be
+distributed under the same license terms.
+ 
+******************************************************************************************************************/
+
+USE [EXM]
+GO
+
+/****** Object:  Table [dbo].[tblPlayerFact]    Script Date: 10/27/2019 3:22:54 PM ******/
+DROP TABLE [dbo].[tblPlayerFact]
+GO
+
+/****** Object:  Table [dbo].[tblPlayerFact]    Script Date: 10/27/2019 3:22:54 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[tblPlayerFact](
+	[as_of_date] [varchar](10) NOT NULL,
+	[pl_id] [int] NOT NULL,
+	[mtd_salary] [float] NOT NULL,
+ CONSTRAINT [PK_tblPlayerFact] PRIMARY KEY CLUSTERED 
+(
+	[as_of_date] ASC,
+	[pl_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+

669b2f414e0624d20967af6610ea02e350b304ec
Jon Jaussi <jon@jaussiconsulting.net>
2019-11-03 19:08:37 -0700

    Add script header

--- other_scripts/HowToCreateMS_Description.sql
+++ other_scripts/HowToCreateMS_Description.sql
@@ -1,3 +1,29 @@
+/*****************************************************************************************************************
+NAME:    HowToCreateMS_Description.sql
+PURPOSE: How to Create MS_Description
+
+MODIFICATION LOG:
+Ver      Date        Author        Description
+-----   ----------   -----------   -------------------------------------------------------------------------------
+1.0     11/03/2019   JJAUSSI       1. Built this table for LDS BC IT243
+
+
+RUNTIME: 
+Approx. 0 sec
+
+NOTES:
+This is an example of how to create a custom propery named MS_Description which is widely used to
+document schemas, tables, and fields in SQL Server. Can also be used to document contraints, indexes, 
+and defualts. See any table in AdventureWorks for an example.
+
+LICENSE: This code is covered by the GNU General Public License which guarantees end users
+the freedom to run, study, share, and modify the code. This license grants the recipients
+of the code the rights of the Free Software Definition. All derivative work can only be
+distributed under the same license terms.
+ 
+******************************************************************************************************************/
+
+
 -- Table
 EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'The Position Dimension table.' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'tblPositionDim';
 

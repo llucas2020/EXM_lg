bca4786326d14ee0a20776513eee323fdf373d68
Jon Jaussi <jon@jaussiconsulting.net>
2020-01-29 20:26:47 -0700

    Enhanced example with Dynamic SQL for tables

--- other_scripts/HowToCreateMS_Description.sql
+++ other_scripts/HowToCreateMS_Description.sql
@@ -6,6 +6,7 @@ MODIFICATION LOG:
 Ver      Date        Author        Description
 -----   ----------   -----------   -------------------------------------------------------------------------------
 1.0     11/03/2019   JJAUSSI       1. Built this table for LDS BC IT243
+1.1     01/29/2020   JJAUSSI       1. Enhanced example
 
 
 RUNTIME: 
@@ -23,6 +24,29 @@ distributed under the same license terms.
  
 ******************************************************************************************************************/
 
+
+-- Q1: How to use dynamic SQL to write SQL for me?
+-- A1: Here is a simple example...
+
+-- This example is for the Tables
+
+SELECT t.TABLE_SCHEMA, t.TABLE_NAME
+,'EXEC sys.sp_addextendedproperty @name=N''MS_Description'', 
+@value=N''The Position Dimension table.'' , 
+@level0type=N''SCHEMA'',@level0name=N''dbo'', 
+@level1type=N''TABLE'',@level1name=N''' + t.TABLE_NAME + ''';' 
+as sql1
+  FROM INFORMATION_SCHEMA.TABLES as t
+ WHERE t.TABLE_TYPE = 'BASE TABLE'
+   AND t.TABLE_SCHEMA = 'dbo'
+ ORDER BY t.TABLE_NAME;
+
+ -- Adapt the script above to use INFORMATION_SCHEMA.COLUMNS for the Columns
+
+
+
+-- Q2: What does the final output look like for this stored procedure that I need to execute?
+-- A2: Here it is - copy and paste from the data grid that results from the prior query
 
 -- Table
 EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'The Position Dimension table.' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'tblPositionDim';
